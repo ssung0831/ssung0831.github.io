f4e45bb0e28d1dff5d66043fa7c1c2c8acfbf580
Samantha Sung <sysung@usc.edu>
2023-05-19 16:28:58 -0700

    experience

---
+++ index.js
@@ -0,0 +1,98 @@
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById("name").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+var i = 0;
+var txt = 'Welcome! My name is Samantha Sung.';
+var speed = 70;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
+
+var experienceSection = document.getElementById("experience");
+var exdemo = document.getElementById("exdemo");
+var txt2 = 'experience';
+var speed = 70;
+
+var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+};
+
+var observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+        if (entry.intersectionRatio > 0.5) {
+            typeWriter2();
+            observer.unobserve(experienceSection);
+        }
+    });
+}, options);
+
+observer.observe(experienceSection);
+
+function typeWriter2() {
+    var j = 0;
+
+    function type() {
+        if (j < txt2.length) {
+            exdemo.innerHTML += txt2.charAt(j);
+            j++;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
